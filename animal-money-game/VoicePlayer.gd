8c750b43a4ea9997c7a14a16d02f3cb5e8a5f4f2
TBrush123 <127967155+TBrush123@users.noreply.github.com>
2025-05-03 18:41:47 +0500

    Delete animal-money-game directory

--- animal-money-game/VoicePlayer.gd
+++
@@ -1,7 +0,0 @@
-extends AudioStreamPlayer2D
-
-
-var isPlaying = false
-# Called when the node enters the scene tree for the first time.
-	
-# Called evdery frame. 'delta' is the elapsed time since the previous frame.
